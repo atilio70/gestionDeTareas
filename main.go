2669836b347ddc008c9c6e7b22a952c9f9672d92
atilio70 <atiliostefani@gmail.com>
2025-01-13 22:19:37 -0300

    Estoy sufriendo

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"task-manager-api/db"
 	"task-manager-api/handlers"
 
@@ -8,20 +9,26 @@ import (
 )
 
 func main() {
+	// Inicializar la base de datos
 	db.InitDB()
 
+	// Crear una nueva instancia del router de Gin
 	r := gin.Default()
 
-	//Cargar templates
+	// Cargar los archivos de plantillas desde la carpeta "templates"
 	r.LoadHTMLGlob("templates/*")
 
-	//Endpoints principales
-	r.GET("/", handlers.IndexHandler)
-	r.GET("/tasks/create", handlers.CreateFormHandler)
-	r.POST("/tasks/create", handlers.CreateHandler)
-	r.GET("/tasks/edit/:id", handlers.EditFormHandler)
-	r.POST("/tasks/update/:id", handlers.UpdateHandler)
-	r.POST("/tasks/delete/:id", handlers.DeleteHandler)
+	// Definir las rutas principales
+	r.GET("/", handlers.IndexHandler)                       // Página principal que muestra las tareas
+	r.GET("/tasks/create", handlers.CreateFormHandler)      // Formulario para crear nuevas tareas
+	r.POST("/tasks/create", handlers.CreateHandler)         // Endpoint para crear una tarea
+	r.GET("/tasks/edit/:id", handlers.EditFormHandler)      // Formulario para editar tareas
+	r.POST("/tasks/update/:id", handlers.UpdateHandler)     // Endpoint para actualizar una tarea
+	r.POST("/tasks/delete/:id", handlers.DeleteHandler)     // Endpoint para eliminar una tarea
+	r.POST("/tasks/complete/:id", handlers.CompleteHandler) // Endpoint para completar una tarea
 
-	r.Run("0.0.0.0:8080")
+	// Ejecutar el servidor en el puerto 8080
+	if err := r.Run("0.0.0.0:8080"); err != nil {
+		log.Fatal("Error al ejecutar el servidor:", err)
+	}
 }
